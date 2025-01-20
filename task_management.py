6c1593b6886fcaa796a0a109050ff2d39e17809b
Melike Demiray <melikedemirayy23@gmail.com>
2025-01-21 00:30:14 +0100

    Uptade task_management.py

--- task_management.py
+++ task_management.py
@@ -1,1 +1,14 @@
-# add_task(task) => parametre simdilik kullanilabilir daha sonra taskden gelcek chief den gecici string gonderildi
+class TaskManagement:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self,task):
+        if task not in self.tasks:
+            self.tasks.append(task)
+            print("Task added successfully!")
+
+    def print_tasks(self):
+        if not self.tasks:
+            print("No tasks available.")
+        for task in self.tasks:
+            print(task)
