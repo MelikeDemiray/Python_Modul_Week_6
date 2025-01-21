e469ce4ae620af37a0feb78893bd58ddfd835d78
Melike Demiray <melikedemirayy23@gmail.com>
2025-01-21 16:56:21 +0100

    Update task_management.py

--- task_management.py
+++ task_management.py
@@ -7,7 +7,7 @@ class TaskManagement:
             self.tasks.append(task)
             print("Task added successfully!")
 
-    def print_tasks(self):
+    def display_tasks(self):
         if not self.tasks:
             print("No tasks available.")
         for task in self.tasks:
